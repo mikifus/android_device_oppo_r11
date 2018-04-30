d8723d8861a26b0394958db240690f48730b265a
flo071 <flo.hintermeier@gmail.com>
2018-04-30 15:20:49 +0200

    fix issue

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,10 +169,6 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
-TARGET_EXFAT_DRIVER := exfat
-
 # GPS
 USE_DEVICE_SPECIFIC_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
